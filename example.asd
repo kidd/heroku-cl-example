d6355137740d4d6ffa44023a4447bf622f38f5a5
Raimon Grau <raimonster@gmail.com>
2012-04-06 17:53:41 +0200

    added roboshop

--- example.asd
+++ example.asd
@@ -3,9 +3,11 @@
   :description "Example cl-heroku application"
   :depends-on (#:hunchentoot
 	       #:cl-who
+               #:drakma
+               #:closure-html
+               #:cxml-stp
 	       #:postmodern)
   :components ((:file "package")
 	       (:module :src
-			:serial t      
+			:serial t
 			:components ((:file "hello-world")))))
-
--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -22,6 +22,44 @@ TODO: cleanup code."
 (push (hunchentoot:create-folder-dispatcher-and-handler "/static/" "/app/public/")
 	 hunchentoot:*dispatch-table*)
 
+(defvar *iberlibro* "http://www.iberlibro.com/servlet/SearchResults")
+
+(defun find-price (div)
+  (let (price-str (price 0))
+   (stp:do-recursively (elem div)
+     (when (and (typep elem 'stp:element)
+                (equal (stp:local-name elem) "span")
+                (equal (stp:attribute-value elem "class") "price"))
+       (setq price-str (stp:data (stp:first-child elem)))
+       (setq price (+ price (read-from-string (subseq price-str 4))))))
+   (format t "preu final: ~d~%" price)))
+
+;;; http://www.iberlibro.com/servlet/SearchResults?sts=t&tn=lisp+in+small+pieces&x=0&y=0
+(defun show-iberlibro-hits (term)
+  (let* ((query (list (cons "tn" term)))
+         (str (drakma:http-request *iberlibro* :parameters query))
+         (document (chtml:parse str (cxml-stp:make-builder))))
+    (stp:do-recursively (a document)
+      (when (and (typep a 'stp:element)
+                 (equal (stp:local-name a) "div")
+                 (equal (stp:attribute-value a "class") "result-addToBasketContainer"))
+        (return (find-price a))))))
+
+
+;; (defvar *preferences* '(("thinking forth" . 7)))
+;; (format t "~{~a ~}" (cdr *posix-argv*))
+(show-iberlibro-hits (format nil "~{~a ~}" (cdr *posix-argv*)))
+
+
+(hunchentoot:define-easy-handler (hello-sbcl :uri "/books") ()
+  (cl-who:with-html-output-to-string (s)
+    (:html
+     (:head
+      (:title "prices"))
+     (:body
+      (:h1 (show-iberlibro-hits "thinking forth")))
+     )))
+
 (hunchentoot:define-easy-handler (hello-sbcl :uri "/") ()
   (cl-who:with-html-output-to-string (s)
     (:html
